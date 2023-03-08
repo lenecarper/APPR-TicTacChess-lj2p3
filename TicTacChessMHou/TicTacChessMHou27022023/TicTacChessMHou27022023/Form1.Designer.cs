ee29b20d5cc9c5f11c6efde64eb25ee4214b9501
Lene <lenecarpertunnel@gmail.com>
2023-03-08 08:42:20 +0100

    Initial commit

---
+++ TicTacChessMHou/TicTacChessMHou27022023/TicTacChessMHou27022023/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace TicTacChessMHou27022023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxBoardMHou = new System.Windows.Forms.GroupBox();
+            this.btnRestartGameMHou = new System.Windows.Forms.Button();
+            this.lblStartGameMHou = new System.Windows.Forms.Label();
+            this.gbxPiecesMHou = new System.Windows.Forms.GroupBox();
+            this.rdbWhiteMHou = new System.Windows.Forms.RadioButton();
+            this.rdbBlackMHou = new System.Windows.Forms.RadioButton();
+            this.pbxPieceThreeMHou = new System.Windows.Forms.PictureBox();
+            this.pbxPieceTwoMHou = new System.Windows.Forms.PictureBox();
+            this.pbxPieceOneMHou = new System.Windows.Forms.PictureBox();
+            this.pbxNineMHou = new System.Windows.Forms.PictureBox();
+            this.pbxEightMHou = new System.Windows.Forms.PictureBox();
+            this.pbxSevenMHou = new System.Windows.Forms.PictureBox();
+            this.pbxSixMHou = new System.Windows.Forms.PictureBox();
+            this.pbxFiveMHou = new System.Windows.Forms.PictureBox();
+            this.pbxFourMHou = new System.Windows.Forms.PictureBox();
+            this.pbxThreeMHou = new System.Windows.Forms.PictureBox();
+            this.pbxTwoMHou = new System.Windows.Forms.PictureBox();
+            this.pbxOneMHou = new System.Windows.Forms.PictureBox();
+            this.gbxBoardMHou.SuspendLayout();
+            this.gbxPiecesMHou.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPieceThreeMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPieceTwoMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPieceOneMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxNineMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxEightMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSevenMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSixMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFiveMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFourMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxThreeMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTwoMHou)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxOneMHou)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxBoardMHou
+            // 
+            this.gbxBoardMHou.Controls.Add(this.pbxNineMHou);
+            this.gbxBoardMHou.Controls.Add(this.pbxEightMHou);
+            this.gbxBoardMHou.Controls.Add(this.pbxSevenMHou);
+            this.gbxBoardMHou.Controls.Add(this.pbxSixMHou);
+            this.gbxBoardMHou.Controls.Add(this.pbxFiveMHou);
+            this.gbxBoardMHou.Controls.Add(this.pbxFourMHou);
+            this.gbxBoardMHou.Controls.Add(this.pbxThreeMHou);
+            this.gbxBoardMHou.Controls.Add(this.pbxTwoMHou);
+            this.gbxBoardMHou.Controls.Add(this.pbxOneMHou);
+            this.gbxBoardMHou.Location = new System.Drawing.Point(138, 115);
+            this.gbxBoardMHou.Name = "gbxBoardMHou";
+            this.gbxBoardMHou.Size = new System.Drawing.Size(548, 478);
+            this.gbxBoardMHou.TabIndex = 0;
+            this.gbxBoardMHou.TabStop = false;
+            this.gbxBoardMHou.Text = "Board";
+            // 
+            // btnRestartGameMHou
+            // 
+            this.btnRestartGameMHou.Location = new System.Drawing.Point(138, 51);
+            this.btnRestartGameMHou.Name = "btnRestartGameMHou";
+            this.btnRestartGameMHou.Size = new System.Drawing.Size(148, 58);
+            this.btnRestartGameMHou.TabIndex = 1;
+            this.btnRestartGameMHou.Text = "Restart Game";
+            this.btnRestartGameMHou.UseVisualStyleBackColor = true;
+            this.btnRestartGameMHou.Click += new System.EventHandler(this.btnRestartGameMHou_Click);
+            // 
+            // lblStartGameMHou
+            // 
+            this.lblStartGameMHou.AutoSize = true;
+            this.lblStartGameMHou.Location = new System.Drawing.Point(403, 92);
+            this.lblStartGameMHou.Name = "lblStartGameMHou";
+            this.lblStartGameMHou.Size = new System.Drawing.Size(169, 17);
+            this.lblStartGameMHou.TabIndex = 2;
+            this.lblStartGameMHou.Text = "Start game, set up pieces";
+            // 
+            // gbxPiecesMHou
+            // 
+            this.gbxPiecesMHou.Controls.Add(this.pbxPieceThreeMHou);
+            this.gbxPiecesMHou.Controls.Add(this.pbxPieceTwoMHou);
+            this.gbxPiecesMHou.Controls.Add(this.pbxPieceOneMHou);
+            this.gbxPiecesMHou.Location = new System.Drawing.Point(711, 115);
+            this.gbxPiecesMHou.Name = "gbxPiecesMHou";
+            this.gbxPiecesMHou.Size = new System.Drawing.Size(180, 478);
+            this.gbxPiecesMHou.TabIndex = 3;
+            this.gbxPiecesMHou.TabStop = false;
+            this.gbxPiecesMHou.Text = "Pieces";
+            // 
+            // rdbWhiteMHou
+            // 
+            this.rdbWhiteMHou.AutoSize = true;
+            this.rdbWhiteMHou.Checked = true;
+            this.rdbWhiteMHou.Location = new System.Drawing.Point(711, 92);
+            this.rdbWhiteMHou.Name = "rdbWhiteMHou";
+            this.rdbWhiteMHou.Size = new System.Drawing.Size(65, 21);
+            this.rdbWhiteMHou.TabIndex = 4;
+            this.rdbWhiteMHou.TabStop = true;
+            this.rdbWhiteMHou.Text = "White";
+            this.rdbWhiteMHou.UseVisualStyleBackColor = true;
+            this.rdbWhiteMHou.CheckedChanged += new System.EventHandler(this.rdbWhiteMHou_CheckedChanged);
+            // 
+            // rdbBlackMHou
+            // 
+            this.rdbBlackMHou.AutoSize = true;
+            this.rdbBlackMHou.Location = new System.Drawing.Point(828, 92);
+            this.rdbBlackMHou.Name = "rdbBlackMHou";
+            this.rdbBlackMHou.Size = new System.Drawing.Size(63, 21);
+            this.rdbBlackMHou.TabIndex = 5;
+            this.rdbBlackMHou.TabStop = true;
+            this.rdbBlackMHou.Text = "Black";
+            this.rdbBlackMHou.UseVisualStyleBackColor = true;
+            // 
+            // pbxPieceThreeMHou
+            // 
+            this.pbxPieceThreeMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxPieceThreeMHou.BackgroundImage = global::TicTacChessMHou27022023.Properties.Resources.Chess_Knight_White;
+            this.pbxPieceThreeMHou.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbxPieceThreeMHou.Location = new System.Drawing.Point(6, 327);
+            this.pbxPieceThreeMHou.Name = "pbxPieceThreeMHou";
+            this.pbxPieceThreeMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxPieceThreeMHou.TabIndex = 11;
+            this.pbxPieceThreeMHou.TabStop = false;
+            // 
+            // pbxPieceTwoMHou
+            // 
+            this.pbxPieceTwoMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxPieceTwoMHou.BackgroundImage = global::TicTacChessMHou27022023.Properties.Resources.Chess_Rook_White1;
+            this.pbxPieceTwoMHou.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbxPieceTwoMHou.Location = new System.Drawing.Point(6, 172);
+            this.pbxPieceTwoMHou.Name = "pbxPieceTwoMHou";
+            this.pbxPieceTwoMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxPieceTwoMHou.TabIndex = 10;
+            this.pbxPieceTwoMHou.TabStop = false;
+            // 
+            // pbxPieceOneMHou
+            // 
+            this.pbxPieceOneMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxPieceOneMHou.BackgroundImage = global::TicTacChessMHou27022023.Properties.Resources.Chess_Queen_White;
+            this.pbxPieceOneMHou.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbxPieceOneMHou.Location = new System.Drawing.Point(6, 21);
+            this.pbxPieceOneMHou.Name = "pbxPieceOneMHou";
+            this.pbxPieceOneMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxPieceOneMHou.TabIndex = 9;
+            this.pbxPieceOneMHou.TabStop = false;
+            // 
+            // pbxNineMHou
+            // 
+            this.pbxNineMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxNineMHou.Location = new System.Drawing.Point(372, 323);
+            this.pbxNineMHou.Name = "pbxNineMHou";
+            this.pbxNineMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxNineMHou.TabIndex = 8;
+            this.pbxNineMHou.TabStop = false;
+            this.pbxNineMHou.Click += new System.EventHandler(this.ClickTile);
+            // 
+            // pbxEightMHou
+            // 
+            this.pbxEightMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxEightMHou.Location = new System.Drawing.Point(372, 172);
+            this.pbxEightMHou.Name = "pbxEightMHou";
+            this.pbxEightMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxEightMHou.TabIndex = 7;
+            this.pbxEightMHou.TabStop = false;
+            this.pbxEightMHou.Click += new System.EventHandler(this.ClickTile);
+            // 
+            // pbxSevenMHou
+            // 
+            this.pbxSevenMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxSevenMHou.Location = new System.Drawing.Point(372, 21);
+            this.pbxSevenMHou.Name = "pbxSevenMHou";
+            this.pbxSevenMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxSevenMHou.TabIndex = 6;
+            this.pbxSevenMHou.TabStop = false;
+            this.pbxSevenMHou.Click += new System.EventHandler(this.ClickTile);
+            // 
+            // pbxSixMHou
+            // 
+            this.pbxSixMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxSixMHou.Location = new System.Drawing.Point(189, 323);
+            this.pbxSixMHou.Name = "pbxSixMHou";
+            this.pbxSixMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxSixMHou.TabIndex = 5;
+            this.pbxSixMHou.TabStop = false;
+            this.pbxSixMHou.Click += new System.EventHandler(this.ClickTile);
+            // 
+            // pbxFiveMHou
+            // 
+            this.pbxFiveMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxFiveMHou.Location = new System.Drawing.Point(189, 172);
+            this.pbxFiveMHou.Name = "pbxFiveMHou";
+            this.pbxFiveMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxFiveMHou.TabIndex = 4;
+            this.pbxFiveMHou.TabStop = false;
+            this.pbxFiveMHou.Click += new System.EventHandler(this.ClickTile);
+            // 
+            // pbxFourMHou
+            // 
+            this.pbxFourMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxFourMHou.Location = new System.Drawing.Point(189, 21);
+            this.pbxFourMHou.Name = "pbxFourMHou";
+            this.pbxFourMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxFourMHou.TabIndex = 3;
+            this.pbxFourMHou.TabStop = false;
+            this.pbxFourMHou.Click += new System.EventHandler(this.ClickTile);
+            // 
+            // pbxThreeMHou
+            // 
+            this.pbxThreeMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxThreeMHou.Location = new System.Drawing.Point(6, 323);
+            this.pbxThreeMHou.Name = "pbxThreeMHou";
+            this.pbxThreeMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxThreeMHou.TabIndex = 2;
+            this.pbxThreeMHou.TabStop = false;
+            this.pbxThreeMHou.Click += new System.EventHandler(this.ClickTile);
+            // 
+            // pbxTwoMHou
+            // 
+            this.pbxTwoMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxTwoMHou.Location = new System.Drawing.Point(6, 172);
+            this.pbxTwoMHou.Name = "pbxTwoMHou";
+            this.pbxTwoMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxTwoMHou.TabIndex = 1;
+            this.pbxTwoMHou.TabStop = false;
+            this.pbxTwoMHou.Click += new System.EventHandler(this.ClickTile);
+            // 
+            // pbxOneMHou
+            // 
+            this.pbxOneMHou.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxOneMHou.Location = new System.Drawing.Point(6, 21);
+            this.pbxOneMHou.Name = "pbxOneMHou";
+            this.pbxOneMHou.Size = new System.Drawing.Size(167, 145);
+            this.pbxOneMHou.TabIndex = 0;
+            this.pbxOneMHou.TabStop = false;
+            this.pbxOneMHou.Click += new System.EventHandler(this.ClickTile);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 729);
+            this.Controls.Add(this.rdbBlackMHou);
+            this.Controls.Add(this.rdbWhiteMHou);
+            this.Controls.Add(this.gbxPiecesMHou);
+            this.Controls.Add(this.lblStartGameMHou);
+            this.Controls.Add(this.btnRestartGameMHou);
+            this.Controls.Add(this.gbxBoardMHou);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxBoardMHou.ResumeLayout(false);
+            this.gbxPiecesMHou.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPieceThreeMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPieceTwoMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPieceOneMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxNineMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxEightMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSevenMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSixMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFiveMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxFourMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxThreeMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTwoMHou)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxOneMHou)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxBoardMHou;
+        private System.Windows.Forms.Button btnRestartGameMHou;
+        private System.Windows.Forms.Label lblStartGameMHou;
+        private System.Windows.Forms.PictureBox pbxSixMHou;
+        private System.Windows.Forms.PictureBox pbxFiveMHou;
+        private System.Windows.Forms.PictureBox pbxFourMHou;
+        private System.Windows.Forms.PictureBox pbxThreeMHou;
+        private System.Windows.Forms.PictureBox pbxTwoMHou;
+        private System.Windows.Forms.PictureBox pbxOneMHou;
+        private System.Windows.Forms.PictureBox pbxNineMHou;
+        private System.Windows.Forms.PictureBox pbxEightMHou;
+        private System.Windows.Forms.PictureBox pbxSevenMHou;
+        private System.Windows.Forms.GroupBox gbxPiecesMHou;
+        private System.Windows.Forms.PictureBox pbxPieceThreeMHou;
+        private System.Windows.Forms.PictureBox pbxPieceTwoMHou;
+        private System.Windows.Forms.PictureBox pbxPieceOneMHou;
+        private System.Windows.Forms.RadioButton rdbWhiteMHou;
+        private System.Windows.Forms.RadioButton rdbBlackMHou;
+    }
+}
+
---
+++ TicTacChessMHou/TicTacChessMHou27022023/TicTacChessMHou27022023/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacChessMHou27022023
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClickTile(object sender, EventArgs e)
+        {
+            PictureBox pic = sender as PictureBox;
+
+            pic.Image = TicTacChessMHou27022023.Properties.Resources.flappybird;
+        }
+
+        private void rdbWhiteMHou_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbWhiteMHou.Checked)
+            {
+                this.pbxPieceOneMHou.Image = TicTacChessMHou27022023.Properties.Resources.Chess_Queen_White;
+                this.pbxPieceTwoMHou.Image = TicTacChessMHou27022023.Properties.Resources.Chess_Rook_White;
+                this.pbxPieceThreeMHou.Image = TicTacChessMHou27022023.Properties.Resources.Chess_Knight_White;
+            }
+            else
+            {
+                this.pbxPieceOneMHou.Image = TicTacChessMHou27022023.Properties.Resources.Chess_Queen_Black;
+                this.pbxPieceTwoMHou.Image = TicTacChessMHou27022023.Properties.Resources.Chess_Rook_Black;
+                this.pbxPieceThreeMHou.Image = TicTacChessMHou27022023.Properties.Resources.Chess_Knight_Black;
+            }
+        }
+
+        private void btnRestartGameMHou_Click(object sender, EventArgs e)
+        {
+            rdbWhiteMHou.Checked = true;
+            // PictureBox pic = sender as PictureBox;
+
+            // pic.Image = null;
+        }
+    }
+}
